59d3062001547460ea6ad55ad09320038a31e4ee
Humam Alhusaini <humamalhusaini@tutamail.com>
2025-08-31 15:27:51 -0500

    added simple REPL

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,9 @@
-let () = print_endline "Hello, World!"
+
+let rec repl () =
+  print_string ">>> ";
+  let txt = read_line () in
+  match txt with
+  | "exit" -> print_endline "Goodbye!"
+  | _ -> repl ();;
+
+let () = repl ();;
