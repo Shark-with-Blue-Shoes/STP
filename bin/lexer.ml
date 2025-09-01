7733195af752823f0131f5712b9a901c2dbb9163
Humam Alhusaini <humamalhusaini@tutamail.com>
2025-08-31 20:31:10 -0500

    added lexing for nums

--- bin/lexer.ml
+++ bin/lexer.ml
@@ -3,9 +3,38 @@ exception Lexing_error of string;;
 open Printf
 open Tokens
 
+let is_alpha = function 'a' .. 'z' | 'A' .. 'Z' -> true | _ -> false
+let is_digit = function '0' .. '9' -> true | _ -> false
+let at_eof pos txt : bool = pos >= String.length txt;;
+
+let string_of_chars chars = 
+  let buf = Buffer.create 16 in
+  List.iter (Buffer.add_char buf) chars;
+  Buffer.contents buf;;
+
 let rec tokenize (txt : string) (pos : int) (tokens : Tokens.t list) : Tokens.t list =
-  if pos < String.length txt then begin
+
+  let rec tokenize_num (chars : char list) pos =
+    if at_eof pos txt |> not then begin
+      if is_digit txt.[pos] then 
+        tokenize_num (txt.[pos] :: chars) (pos+1)
+      else if is_alpha txt.[pos] then 
+        raise (Lexing_error "Can't end number with letter, add a space or something")
+      else 
+        let final_num = List.rev chars |> string_of_chars |> int_of_string in
+          let token = Num final_num in
+          tokenize txt (pos) (token :: tokens) end 
+    else begin
+      let final_num = List.rev chars |> string_of_chars |> int_of_string in
+        let token = Num final_num in
+            (EOF :: token :: tokens) |> List.rev end
+          in
+
+  if at_eof pos txt |> not then begin
     let char = txt.[pos] in
+      if is_digit char then
+        tokenize_num [char] (pos+1)
+      else
       match char with
       | '+' -> tokenize txt (pos+1) (PLUS :: tokens)
       | '/' -> tokenize txt (pos+1) (DIV :: tokens)
--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,4 @@
 open Lexer
-open Printf
 
 let rec repl () =
   print_string ">>> ";
@@ -7,7 +6,6 @@ let rec repl () =
   match txt with
   | "exit" -> print_endline "Goodbye!"
   | str -> 
-    printf "input is %i long\n" (String.length str);
     let tokens = tokenize str 0 [] in
                 print_tokens tokens;
       repl ();;
