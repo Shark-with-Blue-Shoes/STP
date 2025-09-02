30c69ec04f9b9b29a599abb25cb9ae3619280e22
Humam Alhusaini <humamalhusaini@tutamail.com>
2025-09-02 13:47:00 -0500

    fixed dune files, am prepared for a test but for whatever reason, dune test cannot find a file whether it is in test/ or at the top

---
+++ test/test_STP.ml
@@ -0,0 +1,12 @@
+open STP.Lexer
+
+let read_file file : string = In_channel.with_open_bin file In_channel.input_all;;
+
+let interp file = 
+  print_string "interpreting file...\n";
+  let str = read_file file in
+    let lex = new lexer str in
+      let _ = lex#tokenize in
+      lex#pretty_print;;
+
+let () = interp "test.stp";;
