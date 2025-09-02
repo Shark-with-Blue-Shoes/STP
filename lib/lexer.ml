ca4280ca98ea80e3730fcaa5d9c93a98d6c55ea5
Humam Alhusaini <humamalhusaini@tutamail.com>
2025-09-02 12:16:11 -0500

    classified pos

--- lib/lexer.ml
+++ lib/lexer.ml
@@ -23,38 +23,42 @@ let string_to_tok str : Tokens.t =
   | "definition" -> DEFINITION
   | _ -> Var str;;
 
+class position_tracker txt =
+  object
+    val mutable pos = 0
+
+    method next_pos () =
+      pos <- pos + 1
+
+    method back_pos () =
+      pos <- pos - 1
+
+    method current_pos = pos
+
+    method at_eof () : bool = pos >= String.length txt
+  end
+
 class lexer (str : string) = object (self)
   (*The string being lexed*)
   val txt : string = str
  
-  (*Position is origiinally 0 when initalized*)
-  val mutable pos = 0
-
-  (*Increment pos*)
-  method next_pos () = 
-    pos <- pos+1
-
-  (*decrement pos*)
-  method back_pos () = 
-    pos <- pos-1
- 
+  val pos_tracker = new position_tracker str 
   val mutable tokens = []
   
   (*Tokenizes txt*)
   method tokenize : unit =
 
-  let tokenize_next () = self#next_pos (); self#tokenize in
+  let tokenize_next () = pos_tracker#next_pos (); self#tokenize in
 
-  let at_eof () : bool = pos >= String.length txt in
   (*This tokenizes numbers, it is initiated when the lexer finds a num*)
   let rec tokenize_num (chars : char list) =
-    self#next_pos ();
-    if at_eof () |> not then begin
-      let char = txt.[pos] in
+    pos_tracker#next_pos ();
+    if pos_tracker#at_eof () |> not then begin
+      let char = txt.[pos_tracker#current_pos] in
       match char with
       | 'a' .. 'z' | 'A' .. 'Z' -> Lexing_error "Can't end number with letter, add a space or something" |> raise 
       | '0' .. '9' -> tokenize_num (char :: chars)
-      | _ -> self#back_pos ();
+      | _ -> pos_tracker#back_pos ();
             let final_num = List.rev chars |> string_of_chars |> int_of_string in
             let token = Num final_num in token end
     else
@@ -63,20 +67,20 @@ class lexer (str : string) = object (self)
           in
 
   let rec tokenize_word (chars : char list) : Tokens.t = 
-    self#next_pos ();
-    if at_eof () |> not then begin
-      let char = txt.[pos] in
+    pos_tracker#next_pos ();
+    if pos_tracker#at_eof () |> not then begin
+      let char = txt.[pos_tracker#current_pos] in
       match char with
       | 'a' .. 'z' | 'A' .. 'Z' | '0' .. '9' | '_' -> tokenize_word (char :: chars)
-      | _ -> self#back_pos (); let token = List.rev chars |> string_of_chars |> string_to_tok in token end
+      | _ -> pos_tracker#back_pos (); let token = List.rev chars |> string_of_chars |> string_to_tok in token end
     else 
       let token = List.rev chars |> string_of_chars |> string_to_tok in token
           in
 
   let rec skip_comment () : unit = 
-    self#next_pos ();
-    if at_eof () |> not then begin
-      let char = txt.[pos] in
+    pos_tracker#next_pos ();
+    if pos_tracker#at_eof () |> not then begin
+      let char = txt.[pos_tracker#current_pos] in
       match char with
       | '\\' -> ()
       | _ -> skip_comment ()
@@ -84,9 +88,9 @@ class lexer (str : string) = object (self)
     else 
     Lexing_error "Forgot to close comment" |> raise in
 
-  if at_eof () |> not then begin
+  if pos_tracker#at_eof () |> not then begin
     try
-      let char = txt.[pos] in
+      let char = txt.[pos_tracker#current_pos] in
       match char with
       | ' ' | '\t' | '\n' | '\r' -> tokenize_next ()
       | '\\' -> skip_comment (); tokenize_next ()
@@ -115,7 +119,7 @@ class lexer (str : string) = object (self)
                                   tokenize_next ()
     with 
     | Lexing_error err -> 
-        printf "LEXING ERROR: %s\nat offset: %i\n\n\nPrinting retrieved tokens...\n" err pos; 
+        printf "LEXING ERROR: %s\nat offset: %i\n\n\nPrinting retrieved tokens...\n" err pos_tracker#current_pos; 
         tokens <- List.rev tokens;
     | err -> 
         Printexc.to_string err |> printf "ANOMALY: %s\n\n\nPrinting retrieved tokens...\n\n";
