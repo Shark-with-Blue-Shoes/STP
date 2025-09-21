1b2b22e12eb23433c250701d91a9ebb0fc55cd7f
Humam Alhusaini <humamalhusaini@tutamail.com>
2025-09-20 19:10:43 -0500

    made a nice mini repl within the main repl

--- bin/main.ml
+++ bin/main.ml
@@ -4,12 +4,39 @@ open STP.Printer
 
 open Printf
 
-let run_assistant str = 
+(*Takes a lemma and a string, parses the string and applies the tactic to the lemma*)
+let interp_tactic (str : string) (lemma : lemma) : lemma = 
+  try
+    let lex = new lexer str in
+      let tokens = lex#tokenize [] in
+        let _ = parse_input tokens parse_tactic in lemma 
+  with 
+  | Lexing_error (err, toks, pos) -> 
+      printf "LEXING ERROR at line %d, offset %d: %s\n\n\n" pos.line_num pos.bol_off err;
+      print_string "Printing retrieved tokens...\n\n";
+      print_tokens toks; lemma
+  | Parsing_error (err, tok) -> 
+      let (tok, pos) = tok in
+      let tokstr = format_tok tok in
+      printf "PARSING ERROR: %s at token %s line %d, offset %d\n" err tokstr pos.line_num pos.bol_off;
+      lemma
+  | e -> Printexc.to_string e |> printf "ANOMALY: %s\n"; lemma;;
+
+(*A repl inside the get_lemma repl*)
+let rec prove_lemma (lemma : lemma) : unit = 
+  print_string "*** ";
+  let txt = read_line () in
+  match txt with
+  | "exit" -> print_endline "Goodbye!"
+  | _ -> let _(*New lemma*) = interp_tactic txt lemma in prove_lemma lemma;;
+
+let interp_lemma (str : string) : unit = 
   try
     let lex = new lexer str in
       let tokens = lex#tokenize [] in
         let ast = parse_input tokens parse_lemma in
-          print ast flemma;
+          print_string "Time to prove it!\n";
+          prove_lemma ast;
   with 
   | Lexing_error (err, toks, pos) -> 
       printf "LEXING ERROR at line %d, offset %d: %s\n\n\n" pos.line_num pos.bol_off err;
@@ -22,12 +49,12 @@ let run_assistant str =
   | e -> Printexc.to_string e |> printf "ANOMALY: %s\n";;
 
 
-let rec repl () =
+let rec get_lemma () =
   print_string ">>> ";
   let txt = read_line () in
   match txt with
   | "exit" -> print_endline "Goodbye!"
-  | _ -> run_assistant txt;
-      repl ();;
+  | _ -> interp_lemma txt;
+      get_lemma ();;
 
-let () = repl ();;
+let () = get_lemma ();;
