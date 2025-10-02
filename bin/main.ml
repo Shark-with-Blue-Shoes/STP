df7e18106b4a21e6ebb70106194664750e3f5e3b
Humam Alhusaini <humamalhusaini@tutamail.com>
2025-10-01 19:28:49 -0500

    only repl, no files

--- bin/main.ml
+++ bin/main.ml
@@ -2,8 +2,6 @@ open STP.Lexer
 open STP.Parser
 open STP.Printer
 
-open Printf
-
 let rec repl () =
   print_string ">>> ";
   let txt = read_line () in
@@ -15,20 +13,4 @@ let rec repl () =
             print_expr ast;
       repl ();;
 
-let read_file file : string = In_channel.with_open_bin file In_channel.input_all;;
-
-let interp file = 
-  print_string "interpreting file...\n";
-  let str = read_file file in
-    let lex = new lexer str in
-      let _ = lex#tokenize in
-        let ast = parse_expr lex#get_tokens in
-          print_expr ast;;
-
-let () = 
-  try
-    match Sys.argv.(1) with 
-    | "repl" -> repl ()
-    | str -> interp str
-  with 
-  | Invalid_argument _ -> printf "put a damn argument!";;
+let () = repl ();;
