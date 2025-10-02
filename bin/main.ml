3c9a10e075508660fc0cdbf1262795af6739d31d
Humam Alhusaini <humamalhusaini@tutamail.com>
2025-10-01 19:31:42 -0500

    added interp func

--- bin/main.ml
+++ bin/main.ml
@@ -2,15 +2,17 @@ open STP.Lexer
 open STP.Parser
 open STP.Printer
 
+let interp str =
+  let lex = new lexer str in
+    let _ = lex#tokenize in
+      let ast = parse_expr lex#get_tokens in
+        print_expr ast;;
+
 let rec repl () =
   print_string ">>> ";
   let txt = read_line () in
   match txt with
   | "exit" -> print_endline "Goodbye!"
-  | str -> let lex = new lexer str in
-            let _ = lex#tokenize in
-            let ast = parse_expr lex#get_tokens in
-            print_expr ast;
-      repl ();;
+  | _ -> interp txt; repl ();;
 
 let () = repl ();;
