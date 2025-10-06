c18f42700039584d8b541ae5da83d03417e03661
Humam Alhusaini <humamalhusaini@tutamail.com>
2025-10-06 18:41:46 -0500

    added prover file

---
+++ lib/prover.ml
@@ -0,0 +1,55 @@
+open Lexer
+open Printer
+open Parser
+open Printf
+
+exception Solved_lemma of lemma
+
+let apply_reflexivity (nm, comp) = 
+  match comp with
+  | Eq (expr1, expr2) -> if expr1 = expr2 then Solved_lemma (nm, comp) |> raise else (nm, comp);;
+
+let apply_tactic ast lemma : lemma = 
+  match ast with
+  | Reflexivity -> apply_reflexivity lemma
+
+(*A repl inside the get_lemma repl*)
+let rec prove_lemma (lemma : lemma) : unit = 
+  print_string "*** ";
+  let txt = read_line () in
+  match txt with
+  | "exit" -> print_endline "Not solving lemma anymore!"
+  | _ -> let lex = new lexer txt in
+           let tokens = lex#tokenize [] in 
+             let pars = new parse_tactic tokens in
+               let tac = pars#parse_tactic in
+                 let _ = apply_tactic tac lemma in prove_lemma lemma;;
+
+let interp str =
+  try
+    let lex = new lexer str in
+      let tokens = lex#tokenize [] in 
+        let pars = new parse_lemma tokens in
+          let (nme, comp) = pars#parse_lemma in
+            printf "\nSolving %s Lemma\n" nme;
+            prove_lemma (nme, comp) 
+  with 
+  | Parsing_error err -> printf "%s\n" err
+  | Lexing_error (err, toks, pos) -> 
+      printf "LEXING ERROR at line %d, offset %d: %s\n\n\n" pos.line_num pos.bol_off err;
+      print_string "Printing retrieved tokens...\n\n";
+      print_tokens toks;;
+
+
+let rec get_lemma () =
+  print_string ">>> ";
+  let txt = read_line () in
+  match txt with
+  | "exit" -> print_endline "Goodbye!"
+  | _ -> try 
+         interp txt; get_lemma ();
+         with
+         | Solved_lemma (nm, _) -> printf "\nSolved lemma %s! Do another!\n" nm; get_lemma ()
+         | e -> Printexc.to_string e |> printf "ANOMALY: %s\n"; get_lemma ();;
+
+
